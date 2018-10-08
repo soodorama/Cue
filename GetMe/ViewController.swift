708ac59c00fbee2db812019db5e70d3566871d04
Neil Sood <soodorama@gmail.com>
2018-10-07 21:00:45 -0700

    initial storyboard

---
+++ GetMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainVC.swift
+//  GetMe
+//
+//  Created by Neil Sood on 10/7/18.
+//  Copyright © 2018 Neil Sood. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
